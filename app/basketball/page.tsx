b710ea163242b0b3db9fd4306bdbec1c71e6541b
techverdictpro <krastios@gmail.com>
2026-07-27 22:50:29 +0300

    Update page.tsx

--- app/basketball/page.tsx
+++ app/basketball/page.tsx
@@ -108,6 +108,7 @@ export default function BasketballPage() {
           <a href="/tennis/">Tennis</a>
           <a href="/nfl/">NFL</a>
           <a href="/tips-today/">Today&apos;s Tips</a>
+          <a href="/results/">Results</a>
         </div>
         <a href="/tips-today/" className="nav-cta">FREE PICKS</a>
       </nav>
